fe422a3a38ebe51c5b304ce40e7bd23d3a0f9b0c
TomorrowStars <chenhongsheng2012@163.com>
2017-11-27 10:32:56 +0800

    20171127_chen_表单处理实例

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from './../shared/product.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  formModel: FormGroup;
+  categories: string[];
+
+
+  constructor(private productService: ProductService, private router: Router) {
+    let fb = new FormBuilder();
+    this.formModel = fb.group({
+      title: ['', [Validators.minLength(3), Validators.required]],
+      price: [null, this.positiveNumberValidator],
+      category: ["-1"]
+    });
+   }
 
   ngOnInit() {
+    this.categories = this.productService.getAllCategories();
+  }
+
+  positiveNumberValidator(control: FormControl){
+    if(!control.value){
+      return null;
+    }
+    let price = parseInt(control.value);
+    if (price > 0) {
+      return null;
+    } else {
+      return {positiveNumber: true};
+    }
+  }
+
+  onSearch(){
+    if (this.formModel.valid) {
+      console.log(this.formModel.value);
+      this.router.navigate(['/']);
+    }
   }
 
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -7,7 +7,7 @@ export class ProductService {
       new Product(2,"第二个商品",1.99,2.5,"这是第二个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
       new Product(3,"第三个商品",1.99,4.5,"这是第三个商品，是我在学习慕课网Angular入门实战时创建的",["图书"]),
       new Product(4,"第四个商品",1.99,1.5,"这是第四个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","软件设备"]),
-      new Product(5,"第五个商品",1.99,0.5,"这是第五个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
+      new Product(5,"第五个商品",1.99,0.5,"这是第五个商品，是我在学习慕课网Angular入门实战时创建的",["硬件设备","硬件设备"]),
       new Product(6,"第六个商品",1.99,3.5,"这是第六个商品，是我在学习慕课网Angular入门实战时创建的",["图书"])
     ];
   
@@ -20,6 +20,10 @@ export class ProductService {
     new Comment(3, 3, "2017-11-13 17:06", "陈升", 4, "东西不错。")
   ]
   constructor() { }
+
+  getAllCategories(): string[]{
+    return ["电子产品", "图书", "硬件设备"];
+  }
 
   getProducts(): Product[]{
     return this.products;
